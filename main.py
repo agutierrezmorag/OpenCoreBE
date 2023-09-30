dfe8efe84f03d07cfae9980a5bb5e69977cce01e
José Sandoval <josesand99v@gmail.com>
2023-09-30 19:21:45 +0000

    add overwrite parameter to json creation function

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
                 html = clean_html(html)
                 news_list = news_collector(html, 3, website)
                 if news_list:
-                    save_to_json(news_list, 'newsdb.json', os.path.join(os.path.dirname(__file__), 'openCore'))
+                    save_to_json(news_list, 'newsdb.json', os.path.join(os.path.dirname(__file__), 'openCore'), overwrite=True)
+                    save_to_json(news_list, 'newsdb_historical.json', os.path.join(os.path.dirname(__file__), 'openCore'))
--- scraper_noticias/data_processing.py
+++ scraper_noticias/data_processing.py
@@ -4,21 +4,28 @@ from scraper_noticias.utils import clean_html
 
 #Archivo donde se guardan las funciones para procesar los datos
 
-def save_to_json(data, filename, path):    
+def save_to_json(data, filename, path, overwrite=False):
     # Check if the provided path exists, create if not
     if not os.path.exists(path):
         os.makedirs(path)
-    
-    # Verifica si el archivo existe, si existe agrega los datos al archivo, si no existe crea el archivo
-    file_path = os.path.join(path, filename)
-    if os.path.exists(file_path):
-        with open(file_path, 'r', encoding='utf-8') as existing_file:
-            existing_data = json.load(existing_file)
-            existing_data.extend(data)
-            with open(file_path, 'w', encoding='utf-8') as file:
-                json.dump(existing_data, file, indent=4, ensure_ascii=False)
-            print(f"Data appended to {filename}")
-    else:
+    #si overwrite es True, sobreescribe el archivo
+    if overwrite:
+        file_path = os.path.join(path, filename)
         with open(file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
         print(f"JSON saved to {filename}")
+    # Verifica si el archivo existe, si existe agrega los datos al archivo, si no existe crea el archivo
+    else:
+        file_path = os.path.join(path, filename)
+        if os.path.exists(file_path):
+            with open(file_path, 'r', encoding='utf-8') as existing_file:
+                existing_data = json.load(existing_file)
+                existing_data.extend(data)
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    json.dump(existing_data, file, indent=4, ensure_ascii=False)
+                print(f"Data appended to {filename}")
+        else:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
+            print(f"JSON saved to {filename}")
+
