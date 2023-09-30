b6e07e3377dfec84af40263fb2c586ff0787dea2
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-09-30 14:51:41 -0300

    loadnews command timezone fix

--- openCore/news/management/commands/loadnews.py
+++ openCore/news/management/commands/loadnews.py
@@ -1,5 +1,6 @@
-import json, os
+import json
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 from news.models import News
 
 
@@ -13,9 +14,14 @@ class Command(BaseCommand):
             with open(json_file, 'r', encoding='utf-8') as file:
                 data = json.load(file)
             for item in data:
+                date_published_str = item['date']
+                date_published = timezone.make_aware(
+                    timezone.datetime.strptime(date_published_str, '%Y-%m-%d %H:%M:%S'),
+                    timezone=timezone.get_current_timezone()
+                )
                 news_item = News(
                     title=item['title'],
-                    date_published=item['date'],
+                    date_published=date_published,
                     content=item['content'],
                     website=item['website'],
                     link=item['link']
@@ -23,6 +29,9 @@ class Command(BaseCommand):
                 news_item.save()
 
             self.stdout.write(self.style.SUCCESS('Data loaded successfully into the database'))
+
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR('File not found.'))
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Error loading data. Error {str(e)}'))
