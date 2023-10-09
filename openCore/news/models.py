8128fea90dad6ebacdcf04fdbb2e5a3462482aa2
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-10-08 22:10:35 -0300

    news model update

--- openCore/news/models.py
+++ openCore/news/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from djongo import models
+from django import forms
 from django.utils import timezone
 
 
@@ -11,6 +12,7 @@ class News(models.Model):
         (NEUTRO, 'Neutro'),
         (NEGATIVO, 'Negativo'),
     ]
+
     title = models.CharField(max_length=150, blank=False)
     date_published = models.DateTimeField(default=timezone.now)
     date_pulled = models.DateTimeField(auto_now_add=True)
@@ -19,6 +21,7 @@ class News(models.Model):
     website = models.CharField(max_length=50)
     link = models.URLField()
     sentiment = models.CharField(max_length=15, default='Neutro', choices=SENTIMENT_CHOICES)
+    indexed_on = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         indexes = [
@@ -27,3 +30,28 @@ class News(models.Model):
 
     def __str__(self):
         return f'{self.title} ({self.website})'
+
+
+class ImportanceScore(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    article_id = models.IntegerField()
+    frequency = models.IntegerField(null=True, blank=True)
+
+
+class ImportanceScoreForm(forms.ModelForm):
+    class Meta:
+        model = ImportanceScore
+        fields = ['article_id', 'frequency']
+
+
+class WordIndex(models.Model):
+    word = models.CharField(max_length=50)  # Store the word itself
+    news = models.ManyToManyField('News') # Reference to the News model
+    frequency_global = models.PositiveIntegerField(null=True, blank=True)
+    importance_scores = models.EmbeddedField(
+        model_container=ImportanceScore,
+        model_form_class=ImportanceScoreForm,
+        null=True,
+        blank=True
+    )
+    tf_idf_score = models.FloatField(default=0.0)
