dad5f066f136b53e81b9b2381659ec8f408fce51
José Sandoval <josesand99v@gmail.com>
2023-10-17 17:19:45 +0000

    improve bun script

--- scraper_noticias/scraper_bun/index.js
+++ scraper_noticias/scraper_bun/index.js
@@ -1,15 +1,28 @@
 import { extract } from '@extractus/article-extractor'
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-//imort news_links.json and iterate over it
-const news_links = JSON.parse(fs.readFileSync('./bun_jsons/news_links.json'))
-const articles = []
-for (const link of news_links) {
-    //extract the article
-    const article = await extract(link)
-    //add the article to the array
-    articles.push(article)
-}
+const scriptDirectory = __dirname;
 
-//save the articles in a json file
-fs.writeFileSync('./bun_jsons/articles.json', JSON.stringify(articles))
+const newsLinksFilePath = path.join(scriptDirectory, 'bun_jsons/news_links.json');
+
+const news_links = JSON.parse(fs.readFileSync(newsLinksFilePath));
+const articles = [];
+
+const extractAndPushArticle = async (link) => {
+  const article = await extract(link);
+  articles.push(article);
+};
+
+const extractPromises = news_links.map(extractAndPushArticle);
+
+Promise.all(extractPromises)
+  .then(() => {
+    // Save the articles in a JSON file
+    const articlesFilePath = path.join(scriptDirectory, 'bun_jsons/articles.json');
+    fs.writeFileSync(articlesFilePath, JSON.stringify(articles));
+  })
+  .catch((error) => {
+    console.error('Error extracting articles:', error);
+  });
+
