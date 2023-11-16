ad82c812b4cc49cffe252e498f2a5a3edb0e1984
José Sandoval <josesand99v@gmail.com>
2023-11-16 16:40:34 +0000

    Add historical index functionality to indexer

--- indexador/main.py
+++ indexador/main.py
@@ -2,11 +2,15 @@ import os
 from indexer import indexer
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from scraper_noticias.data_processing import save_to_json
+from scraper_noticias.data_processing import save_to_json, read_json
 
 if __name__ == "__main__":
     palabras = []
-    indexador = indexer()
+    #historical json will look for the index_historical.json file in the results folder
+    historical_json = None
+    if os.path.exists(os.path.join(os.path.dirname(__file__), 'results', 'index_historical.json')):
+        historical_json = read_json('index_historical.json', os.path.join(os.path.dirname(__file__), 'results'))
+    indexador = indexer(historical_json)
     palabras.extend(indexador)
     save_to_json(palabras, 'index.json', os.path.join(os.path.dirname(__file__), 'results'), overwrite=True)
     save_to_json(palabras, 'index_historical.json', os.path.join(os.path.dirname(__file__), 'results'))
