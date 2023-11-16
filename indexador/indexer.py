40641498898f63fd8fe90f3fcf5c82ce23491484
José Sandoval <josesand99v@gmail.com>
2023-11-16 16:40:22 +0000

    Add text processing functionality to indexer.py

--- indexador/indexer.py
+++ indexador/indexer.py
@@ -3,6 +3,7 @@ import time
 from django.db import transaction
 import bson
 import django
+import re
 from django.utils import timezone  # Import Django's timezone module
 import os
 import sys
@@ -12,7 +13,24 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openCore.settings")
 django.setup()
 from news.models import News, WordIndex, ImportanceScore
 
-def indexer():
+def process_text(input_text):
+    # Convert to lowercase
+    lowercase_text = input_text.lower()
+
+    # Remover acento
+    no_accent_text = lowercase_text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace(
+        'ú', 'u')
+    
+    # Remove punctuation
+    no_punctuation_text = re.sub(r'[^\w\s]', '', no_accent_text)
+
+    # Convert ñ to n
+    final_text = re.sub(r'ñ', 'n', no_punctuation_text)
+
+    return final_text
+
+def indexer(historical_json = None):
+    #regex to convert to lowercase, remove punctuation and convert ñ to n
     print('current time: ', timezone.now())
     #carga el modelo de lenguaje español
     nlp = spacy.load("es_core_news_sm")
@@ -27,7 +45,14 @@ def indexer():
     #cargado de stop words
     stop_words = spacy.lang.es.stop_words.STOP_WORDS
 
-    word_list_to_save = []
+    #word list to save will be an empty array if no historical json is passed
+    #if historical json is passed, it will hold the json data
+    if historical_json is None:
+        word_list_to_save = []
+    else:
+        word_list_to_save = historical_json
+
+    articles_word_count = {}
     for news_article in new_news_articles:
         #tokeniza, elimina las palabras vacías y pone en minúsculas las palabras en los documentos
         content = news_article.content
@@ -36,36 +61,44 @@ def indexer():
         #lista de palabras filtradas(sin stop words)
         words = [token.text.lower() for token in doc if token.text.lower() not in stop_words and token.is_alpha]
         words = list(set(words)) #elimina duplicados
+        articles_word_count[news_article.id] = len(words)
 
         db_words_list = set(WordIndex.objects.values_list('word', flat=True)) #lista de palabras ya indexadas en la base de datos
         # indexado de palabras en el articulo
         for word in words:
+            word_normalized = process_text(word)
             frequency = content.lower().count(word)
             #si la palabra no esta en la base de datos ni en la lista de palabras a guardar, se agrega a la lista de palabras a guardar
-            if word not in db_words_list and word not in [word["word"] for word in word_list_to_save]:
+            if word_normalized not in db_words_list and word_normalized not in [word["word"] for word in word_list_to_save]:
                 word_list_to_save.append({
-                    "word": word,
+                    "word": word_normalized,
                     "frequency_global": frequency,
                     "importance_scores": [
                         {
-                            "id_noticia": news_article.id,
+                            "article_info": {
+                                "article_id": news_article.id,
+                                "word_count": articles_word_count[news_article.id],
+                            },
                             "frequency": frequency,
                             "tf_idf": 0.0
                         }
                     ],
                 })
-            elif word in [word["word"] for word in word_list_to_save]:
+            elif word_normalized in [word["word"] for word in word_list_to_save]:
                 #if word is already in the list, append a dict to the importance_scores list with the id_noticia and frequency
                 for word_dict in word_list_to_save:
-                    if word_dict["word"] == word:
+                    if word_dict["word"] == word_normalized:
                         word_dict["frequency_global"] += frequency
                         word_dict["importance_scores"].append({
-                            "id_noticia": news_article.id,
+                            "article_info": {
+                                "article_id": news_article.id,
+                                "word_count": articles_word_count[news_article.id],
+                            },
                             "frequency": frequency,
                             "tf_idf": 0.0
                         })
         # Update the record of the last indexed article to the most recent one in the batch
-        update_last_indexed_article(news_article)
+        #update_last_indexed_article(news_article)
         print("indexed article: ", news_article.id)
     return word_list_to_save
     print ('time after query: ', timezone.now())
