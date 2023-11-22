702e92be7c606ec7d594202ba4c70f5ca43c2c3a
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-11-21 22:37:44 -0300

    search query optimization

--- openCore/news/views.py
+++ openCore/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.db.models import Q
 from .models import News
 import json
@@ -28,45 +28,37 @@ def home(request):
 
 
 def search(request):
-    
     path_to_json = '../indexador/results'
-    
-    data = read_json('index_historical.json',path_to_json)
-    
+    data = read_json('index_historical.json', path_to_json)
+
     total_articles = len(data[0]['importance_scores'])
-    
+
     for word_data in data:
-        word = word_data['word']
         word_frequency_global = word_data['frequency_global']
-        
+
         for score in word_data['importance_scores']:
             article_frequency = score['frequency']
             article_word_count = score['article_info']['word_count']
-            
+
             tf = article_frequency / article_word_count
-            
+
             idf = log(1 + (total_articles / word_frequency_global))
-            
+
             tf_idf = tf * idf
-            
+
             score['tf_idf'] = tf_idf
 
-            word_data['importance_scores'] = sorted(word_data['importance_scores'], key=lambda x: x['tf_idf'], reverse=True)
-    
-    querys = request.POST.get('query', None)
-    
-    articles = []
-    
-    querys = querys.split(' ')
-    
-    for query in querys:
-        for word_tfidf in data:
-            imp_word = word_tfidf['word']
-            if query == imp_word:
-                imp_scores = sorted(word_tfidf['importance_scores'], key=lambda x: x['tf_idf'], reverse=True)
-                for imp_score in imp_scores:
-                    articles.append(imp_score['article_info']['article_id'])
-            
-    search_results = News.objects.filter(id__in=articles)
-    
-    return render(request, 'results.html', {'search_results': search_results})
+        word_data['importance_scores'] = sorted(word_data['importance_scores'], key=lambda x: x['tf_idf'], reverse=True)
+
+    queries = request.POST.get('query', None)
+
+    if queries:
+        query_set = set(queries.split(' '))
+        articles = [imp_score['article_info']['article_id'] for word_tfidf in data
+                    for imp_score in word_tfidf['importance_scores']
+                    if word_tfidf['word'] in query_set]
+
+        search_results = News.objects.filter(id__in=articles).order_by('-id')
+        return render(request, 'results.html', {'search_results': search_results})
+    else:
+        return render(request, 'results.html', {'search_results': []})
