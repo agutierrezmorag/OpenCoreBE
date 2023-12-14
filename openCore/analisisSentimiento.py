0946037f8d806efc8ba4b72b3d084a6f9d043464
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-12-14 01:52:52 -0300

    tweaked sentiment analisis

--- openCore/analisisSentimiento.py
+++ openCore/analisisSentimiento.py
@@ -2,65 +2,57 @@ from transformers import pipeline
 import json
 import os
 
-with open('newsdb.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+def initialize_sentiment_analyzer():
+    return pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
 
-def analizar_sentimientos_transformers(texto):
-    clasificador_sentimientos = pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
-    
+def analizar_sentimientos_transformers(texto, clasificador_sentimientos):
     resultado = []
     for fragmento in texto.split("."):
         if len(fragmento) != 0:
-            resultado_fragmento = clasificador_sentimientos(fragmento)
-            resultado.append(resultado_fragmento[0]['label'])
+            try:
+                resultado_fragmento = clasificador_sentimientos(fragmento)
+                resultado.append(resultado_fragmento[0]['label'])
+            except Exception as e:
+                print(f"Error analyzing sentiment: {e}")
     
     return resultado
 
 def valorar(etiqueta_estrellas):
+    estrellas = {'5 stars': 5, '4 stars': 4, '3 stars': 3, '2 stars': 2, '1 star': 1}
+    total_estrellas = sum(estrellas.get(etiqueta, 0) for etiqueta in etiqueta_estrellas)
+    promedio = total_estrellas / len(etiqueta_estrellas)
+    promedio_redondeado = round(promedio)
     
-    cincoStars = etiqueta_estrellas.count('5 stars')
-    cuatroStars = etiqueta_estrellas.count('4 stars')
-    tresStars = etiqueta_estrellas.count('3 stars')
-    dosStars = etiqueta_estrellas.count('2 stars')
-    unoStars = etiqueta_estrellas.count('1 star')
-    """print(cincoStars)
-    print(cuatroStars)
-    print(tresStars)
-    print(dosStars)
-    print(unoStars)
-    """
-    print(f"Tamaño: {len(etiqueta_estrellas)}")
-    promedio = (float(cincoStars*5.0+cuatroStars*4.0+tresStars*3.0+dosStars*2.0+unoStars*1.0))/len(etiqueta_estrellas)
-    print(promedio)
-    print(round(promedio))
-    promedio = round(promedio)
-    return promedio
+    return promedio_redondeado
     
 def determinarValor(valor):
-    if(valor == 1):
+    if valor == 1:
         return "NEGATIVE"
-    elif(valor == 2):
-        return "NEUTRAL"
-    elif(valor == 3):
-        return "NEUTRAL"
-    elif(valor == 4):
+    elif valor == 4 or valor == 5:
         return "POSITIVE"
-    elif(valor == 5):
-        return "POSITIVE"
+    else:
+        return "NEUTRAL"
 
-for item in data:
-    print("------------------------------")
-    print(f"Titulo: {item['title']} ")
-    texto = item['content']
-    # Tu texto
-    #texto = "Durante la mañana de este sábado, 84 familias de la comuna de Santiago recibieron las llaves de sus nuevas viviendas, otorgadas gracias al Fondo Solidario para la Elección de Vivienda del Ministerio de Vivienda y Urbanismo. El complejo “Patios de Copiapó” está ubicado en el corazón del barrio 10 de Julio, en el polígono industrial Matta-Carmen, con departamentos de 3 dormitorios, entre 56 y 70 m2. Incluyen tipologías dúplex y triplex, así como viviendas de una planta con accesibilidad universal, para personas con movilidad reducida y/o discapacidad."
+def main():
+    with open('newsdb.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        
+    clasificador_sentimientos = initialize_sentiment_analyzer()
+    
+    for item in data:
+        print("------------------------------")
+        print(f"Titulo: {item['title']} ")
+        texto = item['content']
+        # Analizar sentimientos
+        resultado_sentimientos_transformers = analizar_sentimientos_transformers(texto, clasificador_sentimientos)
+        promedio = valorar(resultado_sentimientos_transformers)
+        
+        item['sentiment'] = determinarValor(promedio)
+        print(f"El análisis de sentimientos es: {determinarValor(promedio)}")
+        
+    with open('newsdb.json', 'w') as f:
+        json.dump(data, f)
 
-    # Analizar sentimientos
-    resultado_sentimientos_transformers = analizar_sentimientos_transformers(texto)
-    promedio = valorar(resultado_sentimientos_transformers)
-    
-    item['sentiment'] = determinarValor(promedio)
-    print(f"El análisis de sentimientos es: {determinarValor(promedio)}")
-    
-with open('newsdb.json', 'w') as f:
-    json.dump(data, f)
+
+if __name__ == "__main__":
+    main()
