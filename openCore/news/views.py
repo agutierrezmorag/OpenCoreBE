f07eab97b3ecf1b44a539ea885b09dc2c5182c28
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-12-18 20:45:30 -0300

    add stats page and update URLs and views

--- openCore/news/views.py
+++ openCore/news/views.py
@@ -1,80 +1,91 @@
-from django.shortcuts import render
-from collections import defaultdict
-import numpy as np
-from django.core.cache import cache
-from .models import News
 import json
 import os
+from collections import defaultdict
+
+import numpy as np
+from django.core.cache import cache
+from django.shortcuts import render
+
+from .models import News
+
 
 def read_json(filename, path):
     file_path = os.path.join(path, filename)
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
 def home(request):
-    
-    cached_data = cache.get('home_data')
+    cached_data = cache.get("home_data")
     if cached_data:
-        return render(request, 'index.html', cached_data)
-    
-    latest_news = News.objects.order_by('-date_published').first()
-    recent_news = News.objects.order_by('-date_published')[1:21]
-    negative_news = News.objects.filter(sentiment='Negativo')[:4]
-    positive_news = News.objects.filter(sentiment='Positivo')[:4]
-    neutral_news = News.objects.filter(sentiment='Neutro')[:20]
+        return render(request, "index.html", cached_data)
+
+    latest_news = News.objects.order_by("-date_published").first()
+    recent_news = News.objects.order_by("-date_published")[1:21]
+    negative_news = News.objects.filter(sentiment="Negativo")[:4]
+    positive_news = News.objects.filter(sentiment="Positivo")[:4]
+    neutral_news = News.objects.filter(sentiment="Neutro")[:20]
 
     context = {
-        'latest_news': latest_news,
-        'recent_news': recent_news,
-        'negative_news': negative_news,
-        'positive_news': positive_news,
-        'neutral_news': neutral_news,
+        "latest_news": latest_news,
+        "recent_news": recent_news,
+        "negative_news": negative_news,
+        "positive_news": positive_news,
+        "neutral_news": neutral_news,
     }
-    
-    cache.set('home_data', context, timeout=3600)
 
-    return render(request, 'index.html', context)
+    cache.set("home_data", context, timeout=3600)
+
+    return render(request, "index.html", context)
 
 
 def search(request):
-    path_to_json = '../indexador/results'
-    data = cache.get('search_data')
-    
+    path_to_json = "../indexador/results"
+    data = cache.get("search_data")
+
     if data is None:
-        data = read_json('index_historical.json', path_to_json)
+        data = read_json("index_historical.json", path_to_json)
 
-        total_articles = len(data[0]['importance_scores'])
+        total_articles = len(data[0]["importance_scores"])
 
-        idf_values = {word_data['word']: np.log(1 + (total_articles / word_data['frequency_global'])) for word_data in data}
+        idf_values = {
+            word_data["word"]: np.log(
+                1 + (total_articles / word_data["frequency_global"])
+            )
+            for word_data in data
+        }
 
         for word_data in data:
-            for score in word_data['importance_scores']:
-                article_frequency = score['frequency']
-                article_word_count = score['article_info']['word_count']
+            for score in word_data["importance_scores"]:
+                article_frequency = score["frequency"]
+                article_word_count = score["article_info"]["word_count"]
 
                 tf = article_frequency / article_word_count
-                idf = idf_values[word_data['word']]
+                idf = idf_values[word_data["word"]]
                 tf_idf = tf * idf
-                score['tf_idf'] = tf_idf
+                score["tf_idf"] = tf_idf
 
-            word_data['importance_scores'] = sorted(word_data['importance_scores'], key=lambda x: x['tf_idf'], reverse=True)
-        
-        cache.set('search_data', data, timeout=3600)
+            word_data["importance_scores"] = sorted(
+                word_data["importance_scores"], key=lambda x: x["tf_idf"], reverse=True
+            )
 
-    query = request.POST.get('query', '')
+        cache.set("search_data", data, timeout=3600)
+
+    query = request.POST.get("query", "")
     query_words = set(query.lower().split())
 
-    tfidf_words = {word_data['word'] for word_data in data}
+    tfidf_words = {word_data["word"] for word_data in data}
 
     relevant_words = query_words.intersection(tfidf_words)
 
     word_scores = defaultdict(list)
     for word_tfidf in data:
-        if word_tfidf['word'] in relevant_words:
-            importance_scores = word_tfidf['importance_scores']
-            word_scores[word_tfidf['word']].extend(score['article_info']['article_id'] for score in importance_scores)
+        if word_tfidf["word"] in relevant_words:
+            importance_scores = word_tfidf["importance_scores"]
+            word_scores[word_tfidf["word"]].extend(
+                score["article_info"]["article_id"] for score in importance_scores
+            )
 
     article_ids = set(article_id for ids in word_scores.values() for article_id in ids)
 
@@ -82,4 +93,12 @@ def search(request):
 
     total_results = len(search_results)
 
-    return render(request, 'results.html', {'search_results': search_results, 'total_results': total_results})
+    return render(
+        request,
+        "results.html",
+        {"search_results": search_results, "total_results": total_results},
+    )
+
+
+def stats(request):
+    return render(request, "stats.html")
