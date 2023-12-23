1fb99968620132d4d0a8114cecc8abc1c44f69fb
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-12-23 20:26:49 -0300

    made carousel responsive

--- openCore/news/static/js/main.js
+++ openCore/news/static/js/main.js
@@ -1,5 +1,19 @@
-new Glide('.glide').mount();new Glide('.glide', {
+new Glide('.glide', {
   type: 'carousel',
   startAt: 0,
   perView: 5,
+  breakpoints: {
+    1200: {
+      perView: 4
+    },
+    992: {
+      perView: 3
+    },
+    768: {
+      perView: 2
+    },
+    576: {
+      perView: 1
+    }
+  }
 }).mount();
