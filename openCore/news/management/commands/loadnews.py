8ca23c55aea0dc40fe47a4fe1dc8746a55333b6d
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-12-29 11:39:32 -0300

    Refactor news loading logic and switch to PyMongo

--- openCore/news/management/commands/loadnews.py
+++ openCore/news/management/commands/loadnews.py
@@ -1,9 +1,11 @@
 import json
-import time
 
+from decouple import config
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from news.models import News
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from pymongo.server_api import ServerApi
 
 
 class Command(BaseCommand):
@@ -12,7 +14,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         json_file = "newsdb.json"
         print("current time: ", timezone.now())
-        previous_news_uris = [news.link for news in News.objects.all()]
+
+        client = MongoClient(config("MONGO_URI"), server_api=ServerApi("1"))
+        db = client["opencoredatabase"]
+        collection = db["news_news"]
+
+        previous_news_uris = [news["link"] for news in collection.find({})]
         print("time after query: ", timezone.now())
         print(previous_news_uris)
 
@@ -20,7 +27,7 @@ class Command(BaseCommand):
             with open(json_file, "r", encoding="utf-8") as file:
                 data = json.load(file)
             for item in data:
-                if not item["link"] in previous_news_uris:
+                if item["link"] not in previous_news_uris:
                     date_published_str = item["date"]
                     date_published = timezone.make_aware(
                         timezone.datetime.strptime(
@@ -28,19 +35,19 @@ class Command(BaseCommand):
                         ),
                         timezone=timezone.get_current_timezone(),
                     )
-                    news_item = News(
-                        title=item["title"],
-                        date_published=date_published,
-                        content=item["content"],
-                        website=item["website"],
-                        link=item["link"],
-                        img_url=item["image_url"],
-                        sentiment=item["sentiment"],
-                    )
+                    news_item = {
+                        "title": item["title"],
+                        "date_published": date_published,
+                        "content": item["content"],
+                        "website": item["website"],
+                        "link": item["link"],
+                        "img_url": item["image_url"],
+                        "sentiment": item["sentiment"],
+                    }
                     print("------------------------------")
                     print(f"Titulo: {item['title']} ")
                     print(f"Sentimiento: {item['sentiment']} ")
-                    news_item.save()
+                    collection.insert_one(news_item)
 
             self.stdout.write(
                 self.style.SUCCESS("Data loaded successfully into the database")
@@ -49,5 +56,5 @@ class Command(BaseCommand):
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR("File not found."))
 
-        except Exception as e:
+        except PyMongoError as e:
             self.stdout.write(self.style.ERROR(f"Error loading data. Error {str(e)}"))
